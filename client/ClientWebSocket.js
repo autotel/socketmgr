8991fdda7089186c01af4e759882a378330f4037
Joaquin <joaquin@autotel.co>
2016-09-28 20:10:32 +0300

    modularized socket events using on.

--- client/ClientWebSocket.js
+++ client/ClientWebSocket.js
@@ -1,19 +1,17 @@
 export default class ClientWebSocket {
   //should it init in construction, or require an init?
   constructor(){
+    var a=require('./OnHandlers');
+    a.onHandlers.call(this);
     var parent=this;
+    //onHandlers.call(this);
     console.log("ClientWebSocket constructed!")
     var host = window.document.location.host.replace(/:.*/, '');
     this.ws = new WebSocket('ws://' + host + ':8080');
-    this.onMessage=function(data){
-
-      // updateStats(JSON.parse(event.data));
-    }
     this.ws.onmessage = function (event) {
-      parent.onMessage(JSON.parse(event.data));
+      parent.handle("message",JSON.parse(event.data));
     };
   }
   init (){
   }
 }
-
--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 var Wsock=require('./ClientWebSocket').default;
 var wsock=new Wsock();
 wsock.init();
-wsock.onMessage=function(memuse){
-  console.log(memuse);
+wsock.on("message",function(memuse){
+
   document.getElementById('rss').innerHTML = memuse.rss;
   document.getElementById('heapTotal').innerHTML = memuse.heapTotal;
   document.getElementById('heapUsed').innerHTML = memuse.heapUsed;
-};
+});
